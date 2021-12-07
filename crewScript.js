7189fa1e4cd24549e827d7c860adaf7dbee1cce2
Nikhil Pathania <88372381+NikhilPathania18@users.noreply.github.com>
2021-12-07 22:30:37 +0530

    Made this website responsive

--- crewScript.js
+++ crewScript.js
@@ -11,7 +11,7 @@ function commander()
   crewPost.innerHTML="COMMANDER"
   crewName.innerHTML="Douglas Hurley"
   crewDescription.innerHTML='Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.'
-  crewPhoto.innerHTML='<img src="assets/crew/image-douglas-hurley.png" alt="Douglas Harley">'
+  crewPhoto.innerHTML='<img class="crewImage" src="assets/crew/image-douglas-hurley.png" alt="Douglas Harley">'
   setButtonToDefault();
   commanderButton.style.background='white';
 }
@@ -24,7 +24,7 @@ let crewPhoto=document.getElementById('crewPhoto')
   crewPost.innerHTML="FLIGHT ENGINEER"
   crewName.innerHTML="ANOUSHEH ANSARI"
   crewDescription.innerHTML='Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.'
-  crewPhoto.innerHTML='<img src="assets/crew/image-anousheh-ansari.png" alt="">'
+  crewPhoto.innerHTML='<img class="crewImage" src="assets/crew/image-anousheh-ansari.png" alt="">'
   setButtonToDefault();
   
   engineerButton.style.background='white';
@@ -38,7 +38,7 @@ let crewPhoto=document.getElementById('crewPhoto')
   crewPost.innerHTML="PILOT"
   crewName.innerHTML="VICTOR GLOVER"
   crewDescription.innerHTML='Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.';
-  crewPhoto.innerHTML='<img src="assets/crew/image-victor-glover.png" alt="">'
+  crewPhoto.innerHTML='<img class="crewImage" src="assets/crew/image-victor-glover.png" alt="">'
   setButtonToDefault();
   pilotButton.style.background='white';
   
@@ -52,7 +52,7 @@ let crewPhoto=document.getElementById('crewPhoto')
   crewPost.innerHTML="MISSION SPECIALIST"
   crewName.innerHTML="MARK SHUTTLEWORTH"
   crewDescription.innerHTML='Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.'
-  crewPhoto.innerHTML='<img src="assets/crew/image-mark-shuttleworth.png" alt="">'
+  crewPhoto.innerHTML='<img class="crewImage" src="assets/crew/image-mark-shuttleworth.png" alt="">'
   setButtonToDefault();
   specialistButton.style.background='white';
   
--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let homeIcon = document.getElementById("home");
 let destinationIcon = document.getElementById("destination");
 let crewIcon = document.getElementById("crew");
 let technologyIcon = document.getElementById("technology");
+let checkSideBar=0;
 home();
+console.log(window.innerWidth)
 function setIconToDefault() {
   homeIcon.style = "border-bottom: none";
   destinationIcon.style = "border-bottom: none";
@@ -12,8 +14,18 @@ function setIconToDefault() {
 
 function home() {
   let mainDiv = document.getElementById("mainDiv");
-  document.body.style =
-    "background-image: url('assets/home/background-home-desktop.jpg');";
+  if(window.innerWidth<922)
+  {
+    document.body.style ="background-image: url('assets/home/background-home-mobile.jpg');"
+    // document.body.style='background-size: cover'
+  }
+  else if(window.innerWidth<1500&&window.innerWidth>920)
+  {
+    document.body.style ="background-image: url('assets/home/background-home-tablet.jpg');"
+  }
+  else{
+    document.body.style ="background-image: url('assets/home/background-home-desktop.jpg');"
+  }
   mainDiv.innerHTML =
     ' <div class="space"><div class="first"><h2 id="headline">SO, YOU WANT TO TRAVEL TO</h2><h1 id="spaceHeading">SPACE</h1><p id="spacePara">Let' +
     "'" +
@@ -27,8 +39,18 @@ function home() {
 function destination() {
   document.title = "DESTINATION";
   let mainDiv = document.getElementById("mainDiv");
-  document.body.style =
-    "background-image: url('assets/destination/background-destination-desktop.jpg');";
+  if(window.innerWidth<922)
+  {
+    document.body.style ="background-image: url('assets/destination/background-destination-mobile.jpg');"
+    // document.body.style='background-size: cover'
+  }
+  else if(window.innerWidth<1500&&window.innerWidth>920)
+  {
+    document.body.style ="background-image: url('assets/destination/background-destination-tablet.jpg');"
+  }
+  else{
+    document.body.style ="background-image: url('assets/destination/background-destination-desktop.jpg');"
+  }
   mainDiv.innerHTML =
     '<div class="destination" id="destination"><div class="photoMars"><h1 id="pickDestination"><span class="number">01</span> PICK YOUR DESTINATION</h1><div class="planetImage" id="planetImage"><img src="assets/destination/image-titan.png" alt="Moon"></div></div><div class="descriptionSecond" id="descriptionSecond"><div class="navMars"><button class="planets" id="moon" onclick="provideMoon()">MOON</button><button class="planets" id="mars"onclick="provideMars()">MARS</button><button class="planets" id="europa" onclick="provideEuropa()">EUROPA</button><button class="planets" id="titan" onclick="provideTitan()">TITAN</button></div><div class="description" id="description"></div></div></div>';
   provideMoon();
@@ -38,10 +60,37 @@ function destination() {
 function crew() {
   document.title = "CREW";
   let mainDiv = document.getElementById("mainDiv");
-  document.body.style =
-    "background-image: url('assets/crew/background-crew-desktop.jpg');";
+  if(window.innerWidth<922)
+  {
+    document.body.style ="background-image: url('assets/crew/background-crew-mobile.jpg');"
+    // document.body.style='background-size: cover'
+  }
+  else if(window.innerWidth<1500&&window.innerWidth>920)
+  {
+    document.body.style ="background-image: url('assets/crew/background-crew-tablet.jpg');"
+  }
+  else{
+    document.body.style ="background-image: url('assets/crew/background-crew-desktop.jpg');"
+  }
   mainDiv.innerHTML =
-    '<div class="crew"><div class="intro"><h1 id=crewHeading><span id="crewNumber">02</span>MEET YOUR CREW</h1><div class="mainBox"><div class="crewDetails"><div id="crewPost"></div><h1 id="crewName"></h1><p id="crewDescription"></p></div></div><div class="buttonBar"><button class="toggleButton"id="commander" onclick="commander()"></button><button class="toggleButton"id="engineer" onclick="engineer()"></button><button class="toggleButton" id="pilot"onclick="pilot()"></button><button class="toggleButton" id="specialist" onclick="specialist()"></button></div></div><div class="crewPhoto"id="crewPhoto"></div></div>';
+    `<h1 id=crewHeading><span id="crewNumber">02</span>MEET YOUR CREW</h1>
+    <div class="crew">
+    <div class="intro">
+        <div class="mainBox">
+        <div class="crewDetails">
+            <div id="crewPost"></div>
+            <h1 id="crewName"></h1>
+            <p id="crewDescription"></p>
+        </div>
+    </div>
+</div>
+<div class="crewPhoto"id="crewPhoto">
+    
+</div>
+</div>
+<div class="buttonBar">
+    <button class="toggleButton"id="commander" onclick="commander()"></button><button class="toggleButton"id="engineer" onclick="engineer()"></button><button class="toggleButton" id="pilot"onclick="pilot()"></button><button class="toggleButton" id="specialist" onclick="specialist()"></button>
+</div>`;
   setIconToDefault();
   crewIcon.style = "border-bottom: 5px solid white";
   commander();
@@ -50,8 +99,18 @@ function crew() {
 function technology() {
   document.title = "TECHNOLOGY";
   let mainDiv = document.getElementById("mainDiv");
-  document.body.style =
-    "background-image: url('assets/technology/background-technology-desktop.jpg')";
+  if(window.innerWidth<922)
+  {
+    document.body.style ="background-image: url('assets/technology/background-technology-mobile.jpg');"
+    // document.body.style='background-size: cover'
+  }
+  else if(window.innerWidth<1500&&window.innerWidth>920)
+  {
+    document.body.style ="background-image: url('assets/technology/background-technology-tablet.jpg');"
+  }
+  else{
+    document.body.style ="background-image: url('assets/technology/background-technology-desktop.jpg');"
+  }
   mainDiv.innerHTML = `<div class="introBar">
     
     <span id="numberTechnology">03</span>
@@ -73,4 +132,43 @@ function technology() {
   setIconToDefault();
   technologyIcon.style = "border-bottom: 5px solid white";
   firstButton();
+}
+let nav3=document.getElementById('nav3')
+let sideButton=document.getElementById('sideButton')
+
+function sidebarFunction()
+{
+  // console.log('inside sidebar')
+  if(checkSideBar==0)
+  showBar();
+  else
+  closeBar();
+  
+}
+
+function showBar(){
+  let count=300;
+  sideButton.innerHTML=`<img src="assets/shared/icon-close.svg" alt="">`
+
+    let id=setInterval(() => {
+      // console.log('inside this function')
+      nav3.style.right=-1*count+'px'
+      count-=10;
+      if(count<=0)
+      clearInterval(id);
+    }, 5);
+    checkSideBar=1;
+}
+function closeBar(){
+  let count=0;
+  sideButton.innerHTML=`<img src="assets/shared/icon-hamburger.svg" alt="">`;
+  let id=setInterval(() => {
+    nav3.style.right=count+'px'
+    count-=10;
+    if(count<=-300)
+    {
+      clearInterval(id)
+    }
+  }, 5);
+  checkSideBar=0;
 }
